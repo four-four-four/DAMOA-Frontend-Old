18d7c75ca651729d40628533219f85d00cdc4836
0xe82de@gmail.com <0xe82de@gmail.com>
2021-09-25 11:44:44 +0900

    이메일 가입 기능 구조 작성

---
+++ src/views/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <header></header> -->
+  <!-- 로그인 화면 -->
+  <section id="register">
+      <div class="social-account">
+        <div class="social-item" onclick="location.href='EmailRegister'">
+          이메일
+        </div>
+        <div class="social-item" onclick="location.href='http://www.naver.com'">
+          네이버
+        </div>
+        <div class="social-item" onclick="location.href='http://www.kakao.com'">
+          카카오
+        </div>
+        <div class="social-item" onclick="location.href='http://www.google.com/'">
+          구글
+        </div>
+      </div>
+    <article>
+    </article>
+  </section>
+  <!-- <footer></footer> -->
+</template>
