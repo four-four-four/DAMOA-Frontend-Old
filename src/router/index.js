dee8f1193c46fd477f4ee2d9ef6dee6603b253ab
0xe82de@gmail.com <0xe82de@gmail.com>
2021-11-20 18:07:43 +0900

    Update: #19 - User 라우트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,49 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/category',
-    name: 'Category',
+    path: "/user",
+    name: "User",
+    component: () => import("@/views/User.vue"),
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: () => import("@/components/user/Login.vue"),
+      },
+    ],
+  },
+  {
+    path: "/category",
+    name: "Category",
     component: () => import("@/views/Category.vue"),
   },
   {
-    path: '/detail',
-    name: 'Detail',
+    path: "/detail",
+    name: "Detail",
     component: () => import("@/views/Detail.vue"),
   },
   {
-    path: '/authors',
-    name: 'Authors',
+    path: "/authors",
+    name: "Authors",
     component: () => import("@/views/Authors.vue"),
-  }
-]
+  },
+];
 
 const router = new VueRouter({
   routes,
   scrollBehavior() {
-    document.getElementById('app').scrollIntoView();
-  }
-})
+    document.getElementById("app").scrollIntoView();
+  },
+});
 
-export default router
+export default router;
