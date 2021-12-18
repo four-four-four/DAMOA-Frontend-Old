2036ca08d082352d06c7433bf707d22dd8696ee2
유지상 <jidbyoo@gmail.com>
2021-12-18 18:46:51 +0900

    Update: #22 - Vue 기본 구조 수정

---
+++ src/components/user/notice/Notice.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "DamoaFrontendNotice",
+  data() {
+    return { 
+
+    };
+  },
+}
