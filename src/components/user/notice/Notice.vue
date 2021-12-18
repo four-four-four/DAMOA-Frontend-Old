1ef15d8d557f07439242e77dcd4327ef7c20aaba
유지상 <jidbyoo@gmail.com>
2021-12-18 18:47:39 +0900

    Update: #22 - 데이터 속성 추가

--- src/components/user/notice/Notice.vue
+++ src/components/user/notice/Notice.vue
@@ -9,7 +9,21 @@ export default {
   name: "DamoaFrontendNotice",
   data() {
     return { 
-
+      page: 0,
+      active: 0,
+      notice_list: [],
+      notice_list_value: "",
+      notice_list_len: 0,
+      notice_view: [],
+      notice_page: 0,
+      maxNoticeView: 7,
+      add_notice: {
+        title: '',
+        content: ''
+      },
+      del_notice: {
+        notice_id: ''
+      }
     };
   },
 }
