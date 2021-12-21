77867638dbc269fb188eaa4aaaddd41d4b47866f
김경석 <0xe82de@gmail.com>
2021-12-22 05:29:16 +0900

    Update: #20 - 로그인 API 연결

--- src/components/user/signin/SignIn.vue
+++ src/components/user/signin/SignIn.vue
@@ -86,6 +86,25 @@ export default {
     };
   },
 
+  methods: {
+    login() {
+      if (!this.$refs.form.validate()) return;
+
+      http
+        .post(`${this.api}/login`, {
+          userEmail: this.email,
+          userPw: this.password,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log("에러");
+          console.log(error);
+        });
+    },
+  },
+
   beforeCreate() {},
   created() {},
   beforeMount() {},
@@ -94,6 +113,5 @@ export default {
   updated() {},
   beforeUnmount() {},
   unmounted() {},
-  methods: {},
 };
 </script>
