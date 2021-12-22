de3ff53ddfd11b88fcde4a495cd7dea349255739
김경석 <0xe82de@gmail.com>
2021-12-22 23:52:45 +0900

    Update: #20 - vuex로 로그인 API 연결 및 JWT 토큰, ROLE state 관리 로직 추가
    로그인에 성공하면 JWT 토큰과 유저 ROLE을 받아와서 state 변수에 저장합니다.

---
+++ src/store/index.js
@@ -0,0 +1,44 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import http from "@/util/http-common.js";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    jwtToken: null,
+    role: null,
+  },
+  mutations: {
+    SET_JWT_TOKEN(state, payload) {
+      state.jwtToken = payload;
+      console.log(state.jwtToken);
+    },
+    SET_ROLE(state, payload) {
+      state.role = payload;
+      console.log(state.role);
+    },
+  },
+  actions: {
+    signIn({ commit }, userInfo) {
+      return http
+        .post(`/v1/users/login`, {
+          userEmail: userInfo.email,
+          userPw: userInfo.password,
+        })
+        .then(({ data }) => {
+          commit("SET_JWT_TOKEN", data.data.jwtToken);
+          commit("SET_ROLE", data.data.role);
+          return true;
+        })
+        .catch((error) => {
+          console.log("actions.signIn Error");
+          console.log(error);
+          return false;
+        });
+    },
+  },
+  modules: {},
+  plugins: [createPersistedState()],
+});
