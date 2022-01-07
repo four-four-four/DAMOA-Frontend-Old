311460d158aaf18bc1eaaad0124e61a7b25b3fd5
유지상 <jidbyoo@gmail.com>
2022-01-07 16:58:42 +0900

    Create #28 - Mypage 기본 구조 생성

---
+++ src/components/user/mypage/Mypage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "DamoaFrontendMypage",
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  },
+}
+</script>
+<style scoped>
+</style>
