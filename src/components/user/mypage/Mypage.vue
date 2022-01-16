65e1be1ed747c57e331b30e541102533a5051eeb
김경석 <0xe82de@gmail.com>
2022-01-16 21:42:44 +0900

    Create: #31 - 마이페이지 컴포넌트 생성

---
+++ src/components/user/mypage/Mypage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "",
+
+  components: {},
+
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
