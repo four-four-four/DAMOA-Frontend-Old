eae7e322dac9d7ba4bdc81a872afe98a7219f9c0
유지상 <jidbyoo@gmail.com>
2022-02-16 12:33:36 +0900

    Create #30 - 트렌드 기본 구조 생성

---
+++ src/components/main/trend/Trend.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  name: "DamoaFrontendTrend",
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  },
+}
+</script>
+<style scoped>
+}
