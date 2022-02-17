bcf29791f4af2bcb9d8ddb82b1cab835155779d5
유지상 <jidbyoo@gmail.com>
2022-02-17 15:38:50 +0900

    Update: #36 - FontAwesome 추가

--- src/main.js
+++ src/main.js
@@ -12,10 +12,19 @@ import "boxicons/css/boxicons.min.css";
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
 Vue.config.productionTip = false;
 Vue.use(Vuesax, {
   // options here
 });
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueSweetalert2);
 new Vue({
   vuetify,
