cc883e7c7e4e747ce2325807629285100c1bd17c
김경석 <0xe82de@gmail.com>
2022-03-04 23:07:41 +0900

    Common: #42 - 배포 스크립트 추가

--- BAK/README.md
+++
@@ -1,1 +0,0 @@
-# DAMOA-Frontend
---
+++ scripts/frontend-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BE_DIR=/home/ec2-user/deploy/frontend
+
+echo "############################"
+echo "### 프론트엔드 배포 시작 ###"
+echo "############################"
+echo ""
+
+echo "###########################"
+echo "# change build files 시작 #"
+echo "###########################"
+sudo rm -rf /usr/share/nginx/html/dist
+sudo mv $FE_DIR/dist /usr/share/nginx/html/dist
+echo "###########################"
+echo "# change build files 종료 #"
+echo "###########################"
+echo ""
+
+echo "############################"
+echo "### 프론트엔드 배포 종료 ###"
+echo "############################"
+echo ""
